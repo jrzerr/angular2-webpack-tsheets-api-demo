1167c6b3c78200213721ae9ef96e5bc4f4ecc8d2
Jeremy Zerr <jeremy@zerrtech.com>
2016-07-14 17:41:57 -0600

    Added user model

---
+++ src/app/shared/user/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    id: number;
+    username: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+    client_url: string;
+    company_name: string;
+    active: boolean;
+    last_modified: string;
+    created: string;
+}
