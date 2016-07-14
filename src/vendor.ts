80b17915b8633076c8705a8e866b01f90239f967
Jeremy Zerr <jeremy@zerrtech.com>
2016-07-14 17:43:21 -0600

    added rxjs imports, commented out for now

--- src/vendor.ts
+++ src/vendor.ts
@@ -11,3 +11,15 @@ import 'rxjs';
 
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
+//
+//
+// // Statics
+// import 'rxjs/add/observable/throw';
+
+// // Operators
+// import 'rxjs/add/operator/catch';
+// import 'rxjs/add/operator/debounceTime';
+// import 'rxjs/add/operator/distinctUntilChanged';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/toPromise';
