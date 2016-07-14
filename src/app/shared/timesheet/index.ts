3d096c906c0eb8fd791f818b1d770f3620d7edbf
Jeremy Zerr <jeremy@zerrtech.com>
2016-07-14 17:42:40 -0600

    Added timesheet model and initial service and mock json file

---
+++ src/app/shared/timesheet/index.ts
@@ -0,0 +1,2 @@
+export * from './timesheet';
+export * from './timesheet.service';
---
+++ src/app/shared/timesheet/timesheet.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Timesheet } from './timesheet';
+//import { TIMESHEETS } from './mock-timesheets';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class TimesheetService {
+  constructor (private http: Http) {}
+
+  private timesheetsUrl = 'app/timesheets.json';
+
+  getTimesheets(): Observable<Timesheet[]> {
+      return this.http.get(this.timesheetsUrl)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+      let body = res.json();
+      return body.data || {};
+  }
+
+  private handleError (error: any) {
+    let errMsg = (error.message) ? error.message :
+        error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
+}
